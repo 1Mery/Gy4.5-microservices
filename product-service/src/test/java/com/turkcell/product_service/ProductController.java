19a00738f33d00c572ad3fc7c3d0702590b57498
1Mery <meryem.ozevren442001@gmail.com>
2025-10-10 16:04:07 +0300

    update pom.xml

---
+++ product-service/src/test/java/com/turkcell/product_service/ProductController.java
@@ -0,0 +1,15 @@
+package com.turkcell.product_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/products")
+@RestController
+public class ProductController {
+    @GetMapping
+    public String get() {
+        System.out.println("Bu konsoldaki product service çalıştı.");
+        return "Merhaba, Product-Service";
+    }
+}
